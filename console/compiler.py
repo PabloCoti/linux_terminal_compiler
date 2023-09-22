da6c4851878b7d406d17dfd46e31a70081a1b9c5
PabloCoti <pca2003gte@gmail.com>
2023-09-22 00:24:21 -0600

    Custom Man command output for Pablo's

--- console/compiler.py
+++ console/compiler.py
@@ -18,7 +18,8 @@ def compile(command):
         return compile_dynamic_command(command)
 
     else:
-        return 'Error: Command not found.'
+        return 'Error: Comando inexistente.'
+
 
 def compile_dynamic_command(command):
     if any(dynamic_commands in command for dynamic_commands in dynamic_commands):
@@ -38,21 +39,24 @@ def compile_dynamic_command(command):
             return compile_file_command(command, 'mkdir')
 
     else:
-        return 'Error: Command not found.'
+        return 'Error: Comando inexistente.'
+
 
 def compile_file_command(command, dynamic_command):
     if re.fullmatch(r'^\s*{}\s+([a-zA-Z0-9_./-]+\s*)$'.format(dynamic_command), command):
         return run_powershell_command(command)
 
     else:
-        return 'Error: Command not found.'
+        return 'Error: Comando inexistente.'
+
 
 def compile_ls(command):
     if re.fullmatch(r'^\s*ls(\s+-[al])?\s*$', command):
         return run_powershell_command(command)
 
     else:
-        return 'Error: Command not found.'
+        return 'Error: Comando inexistente.'
+
 
 def compile_static_command(command):
     general_pattern = r'^\s*(\w+)\s*$'
@@ -67,20 +71,35 @@ def compile_static_command(command):
         output_command = static_win_equivalent[command_index]
         
         if c_split == 'Man':
-            return "INSTRUCCIONES SOBRE EL USO DE LA APLICACION\n\nNOTA: esta aplicación simula una consola, por lo que ten en cuenta lo siguiente\n\n1. el comando cd emulara un cambio, el proceso no puede ser movido, sin embargo \nse trabajara sobre un path guardado en ram para futuras consultas\n\n2. algunos comandos no funcionan en linux y si en windows, ej. time\n\n------------------------------------------------------------\nCOMANDOS\n    pwd                 muestra el directorio actual, ten en cuenta que la ruta\n                        absoluta no cambiara, pues el proceso no puede cambiar de ruta\n                        pero las consultas que hagas con el programa si se veran reflejados\n                        el puntero será cambiado\n    date                muestra la fecha actual\n    time                muestra la hora actual en la computadora\n    exit                sale del programa\n    clear               limpia la pantalla (consola)\n    Man                 muestra un mensaje con instrucciones e información del programa\n    uname -a            muestra la versión del SO\n    cd <dir>            cambia el path sobre el que estamos trabajando\n    ls [opt] <dir>      muestra los elementos que hay en el directorio\n                        estamos haciendolo basados en linux, por lo que el comando ls no\n                        podra ser ejecutado en windows, es probable que muestre un error\n                        windows utiliza el comando dir\n    rm [files]          borra archivos\n                        files puede ser escrito como un archivo o como un path\n    mkdir <dir>         crea un folder con el nombre de <dir>\n    rmdir <dir>         elimina el folder con el nombre de <dir>, es probable que\n                        no permita borrar carpetas con elementos dentro"
+            return ("INSTRUCCIONES SOBRE EL USO DE LA APLICACION\n\nNOTA: Esta aplicación simula una consola de linux, "
+                    "por lo que ten en cuenta lo siguiente\n\n1. el comando cd simulará un cambio de directorio, "
+                    "el proceso no se va a mover de directorio, sin embargo \nse trabajara sobre un path guardado en "
+                    "ram para futuras consultas\n\n2. Algunos comandos no funcionan en linux y sí en windows o "
+                    "funcionan de forma diferente, ej. time\n\n3. Existen algunos comandos que no funcionarán debido "
+                    "a problemas con permisos que se presentan en windows (como borrar una carpeta con archivos "
+                    "dentro).\n\n------------------------------------------------------------\nCOMANDOS\n    pwd      "
+                    "           Muestra el directorio en el que se está trabajando\n    date                muestra "
+                    "la fecha actual\n    time                Muestra la hora actual en la computadora\n    exit      "
+                    "          Salir del programa\n    clear               Limpiar consola\n    Man                 "
+                    "Instrucciones de manejo del programa\n    uname -a            Versión del sistema operativo\n    "
+                    "cd <dir>            Cambia el path sobre el que estamos trabajando\n    ls [opt] <dir>      "
+                    "muestra los elementos que hay en el directorio\n    rm [file]           Borra archivo "
+                    "especificado\n                        file puede ser escrito como un archivo o como un path\n    "
+                    "mkdir <dir>         Crea un directorio con el nombre de <dir>\n    rmdir <dir>         Elimina "
+                    "el directorio con el nombre de <dir>\nVersión: 1.0\nHecho por: Pablo Cotí - 1653221")
+
         elif c_split == "exit":
             return exit()
 
         return run_cmd_command(output_command)
 
-
-    return 'Error: Command not found.'
+    return 'Error: Comando inexistente.'
 
 
 def run_powershell_command(command):
-    # result = subprocess.run(["powershell", "-Command", command], capture_output=True).stdout
     result = subprocess.run(["powershell", "-Command", command], stdout=subprocess.PIPE, text=True)
     return result.stdout
+
 
 def run_cmd_command(command):
     return system(command)
